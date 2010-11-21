c40e7070e6316290414a61b37b1c9110ed08c2c2
Lucas Satabin <lucassat@n7mm.org>
2010-11-21 21:39:16 +0100

    fix: U must be >: Null

--- src/gnieh/lambda/util/Arm.scala
+++ src/gnieh/lambda/util/Arm.scala
@@ -44,20 +44,20 @@ object Arm {
    * }
    * </pre>
    */
-  def using[T, U <: Closeable](cl: => U)(body: U => T)(implicit handler: Throwable ==> T): T = {
+  def using[T, U >: Null <: Closeable ](cl: => U)(body: U => T)(implicit handler: PartialFunction[Throwable,T]): T = {
     // XXX `truie' is needed because the value of a call-by-name parameter is recomputed at each access
     // so each time we use `cl', we may receive a new instance of U
     // Using a `lazy parameter' (see https://lampsvn.epfl.ch/trac/scala/ticket/240) here is not possible
     // We cannot use `lazy val truie = cl' because we access `truie' in the finally block.
     // If the call to `cl' raises an exception, the same error will be raised in the finally block
-    // because truie was not initialized, and the initialization will be done again, raising the same
+    // because `truie' was not initialized, and the initialization will be done again, raising the same
     // exception.
     var truie: U = null
     try {
       truie = cl
       body(truie)
     } catch {
-      case t if (handler != null) && handler.isDefinedAt(t) =>
+      case t if (handler != null && handler.isDefinedAt(t)) =>
         handler(t)
     }
     finally {
