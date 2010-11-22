837a3f9a04c37ff8b7bb9ce3092bba90c4cdaecf
Lucas Satabin <lucassat@n7mm.org>
2010-11-22 21:03:45 +0100

    reduction strategy interface cleaned up

--- src/gnieh/lambda/strategy/InterpretationStrategy.scala
+++ src/gnieh/lambda/strategy/InterpretationStrategy.scala
@@ -19,22 +19,48 @@ package strategy
 
 import ast._
 
+import org.kiama.rewriting.Rewriter._
+
 /**
  * This trait describes the interface of an interpretation strategy
  * 
  * @author Lucas Satabin
  *
  */
-trait InterpretationStrategy {
-  
+abstract class InterpretationStrategy {
+
   val name: String
-  
-  def init
-  
+
+  /**
+   * Only rewrites an application of a lambda abstraction to a substitution.
+   */
+  protected lazy val step: Strategy = rule {
+    case App(Abs(Var(v), b), p) => Subst(b, v, p)
+  }
+
+  /**
+   * Reduces all substitutions with an outermost strategy
+   */
+  protected lazy val substitute: Strategy = outermost {
+    rule {
+      case Subst(Var(x), y, by) if (x == y) => by
+      case Subst(v: Var, _, _) => v
+      case Subst(App(f, p), v, by) => App(Subst(f, v, by), Subst(p, v, by))
+      case Subst(Abs(Var(x), b), v, by) if v != x => Abs(Var(x), Subst(b, v, by))
+      case Subst(a: Abs, _, _) => a
+    }
+  }
+
+  def stepStrategy: Strategy
+
   /**
    * Reduces the given expression by one step
    * @param expr expression to reduce
    */
-  def apply(expr: LambdaExpr): Option[LambdaExpr]
+  def apply(expr: LambdaExpr): Option[LambdaExpr] =
+    stepStrategy(expr) match {
+      case Some(e: LambdaExpr) => Some(e)
+      case _ => None
+    }
 
 }
--- src/gnieh/lambda/strategy/NormalOrderStrategy.scala
+++ src/gnieh/lambda/strategy/NormalOrderStrategy.scala
@@ -30,27 +30,9 @@ object NormalOrderStrategy extends InterpretationStrategy {
   
   val name = "normal order"
   
-  private lazy val step: Strategy = rule {
-    case App(Abs(Var(v), b), p) => Subst(b, v, p)
-  }
+  /**
+   * Simply reduce the leftmost, outermost lambda term
+   */
+  lazy val stepStrategy: Strategy = oncetd(step) <* substitute
   
-  private lazy val substitute: Strategy = rule {
-    case Subst(Var(x), y, by) if(x == y) => by
-    case Subst(v: Var, _, _) => v
-    case Subst(App(f, p), v, by) => App(Subst(f, v, by), Subst(p, v, by))
-    case Subst(Abs(Var(x), b), v, by) if v != x => Abs(Var(x), Subst(b, v, by))
-    case Subst(a: Abs, _, _) => a
-  }
-  
-  private lazy val stepStrategy = oncetd(step) <* outermost(substitute)
-  
-  def init {
-  }
-
-  def apply(expr: LambdaExpr): Option[LambdaExpr] =
-    stepStrategy(expr) match {
-      case Some(e: LambdaExpr) => Some(e)
-      case _ => None
-    }
-
 }
